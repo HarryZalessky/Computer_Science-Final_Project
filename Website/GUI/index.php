8ce0f5505ff613217c47807aecb566056d7f5307
Harry Zalessky <harryzalessky@hotmail.com>
2017-10-17 13:23:10 +0300

    תחילת בניית האתר

---
+++ Website/GUI/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="/style.css">
+        <title>DevLancer - Developer jobs for freelancers</title>
+    </head>
+    <body>
+        <header>
+            this is header
+        </header>
+    </body>
+</html>
