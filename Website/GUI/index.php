b434cff7dd7c6d8ecb83890c9e277fe18ff6c89b
Harry Zalessky <harryzalessky@hotmail.com>
2017-10-19 18:35:05 +0300

    המשך בניית האתר

--- Website/GUI/index.php
+++ Website/GUI/index.php
@@ -3,11 +3,22 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="/style.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <title>DevLancer - Developer jobs for freelancers</title>
     </head>
     <body>
         <header>
-            this is header
+            <div class="logo">
+                <center>logo</center>
+            </div>
+            <div class="header_content">
+                <div class="header_top">
+                    
+                </div>
+                <div class="header_bottom">
+                    
+                </div>
+            </div>
         </header>
     </body>
 </html>
