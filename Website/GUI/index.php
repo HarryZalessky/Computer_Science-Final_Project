4df5398fb9e444c8f219d161f0fdf0fbc2f52972
Harry Zalessky <harryzalessky@hotmail.com>
2017-10-24 12:10:44 +0300

    המשך בניית הממשק הגרפי של האתר

--- Website/GUI/index.php
+++ Website/GUI/index.php
@@ -7,17 +7,37 @@
         <title>DevLancer - Developer jobs for freelancers</title>
     </head>
     <body>
-        <header>
-            <div class="logo">
-                <center>logo</center>
-            </div>
+        <header class="header_full">
+            <img class="logo" alt="logo" src="/Media/devlancer_vertical.png">
             <div class="header_content">
-                <div class="header_top">
-                    
-                </div>
-                <div class="header_bottom">
-                    
-                </div>
+                <nav class="header_top">
+                    <ul class="left_nav">
+                        <li>
+                            <a class="header_hire">Find Developers</a>
+                        </li>
+                        <li>
+                            <a class="header_work">Find work</a>
+                        </li>
+                    </ul>
+                    <ul class="right_nav">
+                        <li>
+                            <a class="header_register">Become a member</a>
+                        </li>
+                        <li>
+                            <a class="header_login">Already a member? Log In</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav class="header_bottom">
+                    <a class="header_faq">FAQ</a>
+                    <a class="header_community">Community</a>
+                    <a class="header_blog">Blog</a>
+                    <a class="header_about">About Us</a>
+                    <a class="header_contactus">Contact Us</a>
+                    <div class="header_social">
+                        <!--add social media links here-->
+                    </div>
+                </nav>
             </div>
         </header>
     </body>
