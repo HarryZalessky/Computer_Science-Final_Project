682564dea43b96984582389d94695eb30e29e5d8
Harry Zalessky <harryzalessky@hotmail.com>
2017-11-05 08:51:34 +0200

    continued website GUI development

--- Website/GUI/index.php
+++ Website/GUI/index.php
@@ -6,52 +6,7 @@
         <title>DevLancer - Developer jobs for freelancers</title>
     </head>
     <body>
-        <header class="header_full">
-            <img class="logo" alt="logo" src="/Media/devlancer_vertical.png">
-            <div class="header_content">
-                <nav class="header_top">
-                    <ul class="left_nav">
-                        <li class="header_hire">
-                            <a href="Post A Project/index.php">Post A Project</a>
-                        </li>
-                        <li class="header_work">
-                            <a href="Find Work/index.php">Find work</a>
-                        </li>
-                    </ul>
-                    <ul class="right_nav">
-                        <li>
-                            <a href="Register/index.php">Become a member</a>
-                        </li>
-                        <li>
-                            <a href="Login/index.php">Already a member? Log In</a>
-                        </li>
-                    </ul>
-                </nav>
-                <nav class="header_bottom">
-                    <ul class="left_nav">
-                        <li>
-                            <a class="header_faq">FAQ</a>
-                        </li>
-                        <li>
-                            <a class="header_community">Community</a>
-                        </li>
-                        <li>
-                            <a class="header_blog">Blog</a>
-                        </li>
-                        <li>
-                            <a class="header_about">About Us</a>
-                        </li>
-                    </ul>
-                    <ul class="right_nav">
-                        <li>
-                            <a class="header_contactus">Contact Us</a>
-                        </li>
-                        <li class="header_social">
-                            <!--add social media links here-->
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+        <?php require_once '../includes/header.php'; ?>
+        
     </body>
 </html>
