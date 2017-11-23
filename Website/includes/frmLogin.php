e9288c53bea027f1ee3b06ea1d7e2a5209db50a3
Harry Zalessky <HarryZalessky@hotmail.com>
2017-11-23 10:10:12 +0200

    created login and register forms

---
+++ Website/includes/frmLogin.php
@@ -0,0 +1,11 @@
+<div id="frmLogin" class="loginForm">
+    <form autocomplete="on">
+        <div>
+            <input type="text" placeholder="Username or Email" required id="frmLoginUsername"><br />
+            <div></div><br>
+            <input type="password" placeholder="Password" id="frmLoginPassword"><br />
+            <div></div><br>
+            <input type="submit" formmethod="POST">
+        </div>
+    </form>
+</div>
