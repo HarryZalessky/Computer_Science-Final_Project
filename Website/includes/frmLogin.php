a1bd1c2748aa61e9ef889f08da76a745e7c75cd7
Harry Zalessky <HarryZalessky@hotmail.com>
2017-12-07 08:52:50 +0200

    Finished the login form GUI

--- Website/includes/frmLogin.php
+++ Website/includes/frmLogin.php
@@ -1,11 +1,14 @@
 <div id="frmLogin" class="loginForm">
     <form autocomplete="on">
         <div>
-            <input type="text" placeholder="Username or Email" required id="frmLoginUsername"><br />
-            <div></div><br>
-            <input type="password" placeholder="Password" id="frmLoginPassword"><br />
-            <div></div><br>
-            <input type="submit" formmethod="POST">
+            <center><h2>Login</h2></center>
+            <input name="username" type="text" placeholder="Username or Email" required id="frmLoginUsername"><br>
+            <br>
+            <input name="password"type="password" placeholder="Password" required id="frmLoginPassword"><br>
+            <a>Forgot your Password?</a>
+            <br><br>
+            <input name="submit" type="submit" formmethod="POST" class="submit"><br>
+            <span>Not A Member?</span><a href="#" id="registerBtnLogin">Register</a>
         </div>
     </form>
 </div>
