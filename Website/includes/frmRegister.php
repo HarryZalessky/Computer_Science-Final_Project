99b85dd45ad1af47426b6a7d794064770492fdc1
Harry Zalessky <HarryZalessky@hotmail.com>
2017-12-07 08:53:49 +0200

    site dev continued

--- Website/includes/frmRegister.php
+++ Website/includes/frmRegister.php
@@ -1,3 +1,16 @@
 <div id="frmRegister" class="registerForm">
-    
+    <form>
+        <div>
+            <input type="email"><br>
+            <br>
+            <input type="text"><br>
+            <br>
+            <input type="password"><br>
+            <br>
+            <input type="password"><br>
+            <br>
+            <input type="radio" name="action_group" value="work" id="action_group-work"><label for="action_group-work">Work</label>
+            <input type="radio" name="action_group" value="hire" id="action_group-hire"><label for="action_group-hire">Hire</label>
+        </div>
+    </form>
 </div>
--- Website/scripts.js
+++ Website/scripts.js
@@ -1,9 +1,11 @@
 var regFrm = document.getElementById('frmRegister');
 var logFrm = document.getElementById('frmLogin');
-var regBtn = document.getElementById("registerBtn");
-var logBtn = document.getElementById("loginBtn");
-var closeFrmBtn = document.getElementsByClassName("close")[0];
-logBtn.onclick = function() {logFrm.style.display = "block";};
-regBtn.onclick = function() {regFrm.style.display = "block";};
+var regBtn = document.getElementById('registerBtn');
+var logBtn = document.getElementById('loginBtn');
+var regLoginBtn = document.getElementById('registerBtnLogin')
+var closeFrmBtn = document.getElementsByClassName('close')[0];
+logBtn.onclick = function() {logFrm.style.display = 'block';};
+regBtn.onclick = function() {regFrm.style.display = 'block';};
+regLoginBtn.onclick = function() {logFrm.style.display = 'none';regFrm.style.display = 'block'};
 //closeFrmBtn.onclick = function() {logFrm.style.display = "none";regFrm.style.display = "none";};
 window.onclick = function(event) {if(event.target === regFrm){regFrm.style.display = "none";}if(event.target === logFrm){logFrm.style.display = "none";}};
